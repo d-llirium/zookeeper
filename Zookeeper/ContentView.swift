a25d91bb3033848efb99b0b8319211c50fa5ac03
plimacarlos <plimacarlos@brq.com>
2023-01-06 17:05:50 -0300

    List modifiers

--- Zookeeper/ContentView.swift
+++ Zookeeper/ContentView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        List {
-            Text("Item 1")
-            Text("Item 2")
-            Text("Item 3")
+    var body: some View
+    {
+       VStack
+        {
+           Text("I ❤️ List").font(.headline)
+           List {
+               Text("Item 1")
+               Text("Item 2")
+               Text("Item 3")
+           }
+           .listStyle(InsetGroupedListStyle()
+           )
         }
     }
 }
