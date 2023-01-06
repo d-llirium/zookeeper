7134b69fe8db792d1420fba602767f96705d03d0
plimacarlos <plimacarlos@brq.com>
2023-01-06 16:53:35 -0300

    Initial Commit

---
+++ Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+////  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Patricia Carlos on 06/01/23.
+//  
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
