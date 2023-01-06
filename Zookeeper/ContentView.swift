a87b03749529f0f33fe7f25af9b04c26c10c4f6c
plimacarlos <plimacarlos@brq.com>
2023-01-06 17:20:55 -0300

    change animals to a variable and add the @State property wrapper to let the compiler know to change any views that it is used in when its value changes

--- Zookeeper/ContentView.swift
+++ Zookeeper/ContentView.swift
@@ -7,35 +7,45 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View
+{
+    @State var animals = [
+        Animal(
+            name: "Dog"
+        ), Animal(
+            name: "Cat"
+        ), Animal(
+            name: "Parrot"
+        )
+    ]
     var body: some View
     {
-        List(
-            animals
-        ) { animal in
-            Text(
-                animal.name
-            )
+        VStack
+        {
+            List(
+                animals
+            ) { animal in
+                Text(
+                    animal.name
+                )
+            }
+            Button(
+                "Add"
+            ) {
+                animals.append(
+                    Animal(
+                        name: "New Animal"
+                    )
+                )
+            }
         }
     }
-    
 }
 struct Animal: Identifiable
 {
     var id = UUID()
     var name: String
 }
-let animals = [
-    Animal(
-        name: "Dog"
-    ),
-    Animal(
-        name: "Cat"
-    ),
-    Animal(
-        name: "Parrot"
-    )
-]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
