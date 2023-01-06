9ebeaca486ac1af18d5de2f9867ece21c5d7480a
plimacarlos <plimacarlos@brq.com>
2023-01-06 17:27:40 -0300

    using sections on a List

--- Zookeeper/ContentView.swift
+++ Zookeeper/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View
 {
-    @State var animals = [
-        Animal(
-            name: "Dog"
-        ), Animal(
-            name: "Cat"
-        ), Animal(
-            name: "Parrot"
+    let animalGroups = [
+        AnimalGroup(
+            groupName: "Pets",
+            animals: [
+                Animal(name: "Dog"),
+                Animal(name: "Cat"),
+                Animal(name: "Parrot"),
+                Animal(name: "Mouse")
+            ]
+        ),
+        AnimalGroup(
+            groupName: "Farm",
+            animals: [
+                Animal(name: "Cow"),
+                Animal(name: "Horse"),
+                Animal(name: "Goat"),
+                Animal(name: "Sheep"),
+            ]
+        ),
+        AnimalGroup(
+            groupName: "Critters",
+            animals: [
+                Animal(name: "Firefly"),
+                Animal(name: "Spider"),
+                Animal(name: "Ant"),
+                Animal(name: "Squirrel"),
+            ]
         )
     ]
     var body: some View
@@ -23,11 +43,21 @@ struct ContentView: View
         List
         {
             ForEach(
-                animals
-            ) { animal in
-                Text(
-                    animal.name
-                )
+                animalGroups
+            ) { animalGroup in
+                Section(
+                    header: Text(
+                        animalGroup.groupName
+                    )
+                ) {
+                    ForEach(
+                        animalGroup.animals
+                    ) { animal in
+                        Text(
+                            animal.name
+                        )
+                    }
+                }
             }
         }
     }
@@ -36,6 +66,12 @@ struct Animal: Identifiable
 {
     var id = UUID()
     var name: String
+}
+struct AnimalGroup: Identifiable
+{
+    var id = UUID()
+    var groupName : String
+    var animals : [ Animal ]
 }
 
 struct ContentView_Previews: PreviewProvider {
